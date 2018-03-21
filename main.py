d78192667525832e0f6084f210a31250435fa6bf
swarajd <swaraj.dhumne@gmail.com>
2018-03-21 16:20:33 -0400

    added heavy documentation for reference

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
+
 from section import *
+
 from typesection import *
 from importsection import *
 from functionsection import *
@@ -12,17 +14,48 @@ from elementsection import *
 from codesection import *
 from datasection import *
 
-# parse the file into an array of bytes
 def parseFile(filename):
+    """
+        this method reads the file associated with the filename and returns
+        an array of bytes
+
+        = Parameters = 
+        filename    : str        = the name of the file we want to parse
+
+        = Return Value =
+        binaryArray : bytearray  = the array of bytes from the file
+    """
+
+    # the file object from opening the file
     wasmFile = open(filename, "rb")
+
+    # the binary array obtained from reading the file stream
     binaryArray = bytearray(wasmFile.read())
+
+    # return the binary array
     return binaryArray
 
-# our "main"
 def disassemble(filename):
+    """
+        this method disassembles the given filename's file
+
+        = Parameters = 
+        filename : str = the name of the file we want to disassemble
+
+        = Return Value = 
+        NONE
+    """
+
+    # read the file and get the byte array
     binary = parseFile(filename)
+
+    # get the magic number
     magic = binary[0:4]
+
+    # get the version number
     version = int.from_bytes(binary[4:8], byteorder='little')
+
+    # generate the section list from the remaining bytes
     sectionList = makeSectionList(binary[8:])
 
     # Parse each individual sections
@@ -42,7 +75,10 @@ def disassemble(filename):
 
 # code that's only executed if this file itself is run
 if __name__ == '__main__':
+
     # get the filename
     wasmFileName = sys.argv[1]
+
+    # disassemble the file
     disassemble(wasmFileName)
 
--- section.py
+++ section.py
@@ -1,17 +1,62 @@
 class Section:
+    """ This class is a generic class for each section in a .wasm file
+
+    Attributes:
+        sectionCode : int        =  the 'index' for this section
+        sectionSize : int        =  the size in bytes for this section
+        numTypes    : int        =  the number of elements in this section
+        data        : bytearray  =  the rest of the bytes of this section
+    """
     def populate(self, inputBytes):
+        """
+            this method populates the current section with the necessary info,
+            and then returns the remaining unread bytes to be processed later
+
+            = Parameters =
+            inputBytes : bytearray = bytes of wasm file with current section
+
+            = Return Value = 
+            return     : bytearray = bytes of wasm file without current section
+        """
+
+        # one byte for section code
         self.sectionCode = int.from_bytes(inputBytes[0:1], byteorder='little', signed=False)
+
+        # one byte for section size
         self.sectionSize = int.from_bytes(inputBytes[1:2], byteorder='little', signed=False)
+
+        # one byte for the number of types
         self.numTypes    = int.from_bytes(inputBytes[2:3], byteorder='little', signed=False)
+
+        # the rest of the bytes in the current section
         self.data        = inputBytes[3:self.sectionSize+2]
-        print(vars(self))
-        return inputBytes[self.sectionSize+2:] #return remaining bytes
+
+        print(vars(self)) #DEBUG
+
+        # return the rest of the bytes to be processed later
+        return inputBytes[self.sectionSize+2:]
 
 def makeSectionList(inputBytes):
+    """
+        this method creates the section list of twelve sections
+
+        = Parameters =
+        inputBytes  : bytearray   = all of the bytes in the .wasm file
+
+        = Return Value = 
+        sectionList : bytearray[] = an array of sections processed from the 
+                                    array of bytes
+    """
+
+    # 12 sections according to the spec
     sectionList = [None] * 12
+
+    # loop through all 12 sections and populate each if exists
     for i in range (0, len(sectionList)):
         if (inputBytes != None):
             section    = Section()
             inputBytes = section.populate(inputBytes)
             sectionList[section.sectionCode] = section
+
+    # return the generated sectionList
     return sectionList
