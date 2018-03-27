15d11fbfacb99ebd267794379444f354a486b0e3
Vincent Le <vinnyoodles@gmail.com>
2018-03-27 13:51:44 -0400

    Change key type to hex value for LANGUAGE_TYPES

--- constants.py
+++ constants.py
@@ -1,10 +1,9 @@
-
 LANGUAGE_TYPES = {
-    '0x7f' : 'i32',
-    '0x7e' : 'i64',
-    '0x7d' : 'f32',
-    '0x7c' : 'f64',
-    '0x70' : 'anyfunc',
-    '0x60' : 'func',
-    '0x40' : 'pseudo_type'
+    0x7f : 'i32',
+    0x7e : 'i64',
+    0x7d : 'f32',
+    0x7c : 'f64',
+    0x70 : 'anyfunc',
+    0x60 : 'func',
+    0x40 : 'pseudo_type'
 }
