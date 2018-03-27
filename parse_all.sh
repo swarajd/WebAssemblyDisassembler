c9f74801fcbcd519c2bb86a407544c74fb7e0d93
Vincent Le <vinnyoodles@gmail.com>
2018-03-27 11:45:00 -0400

    Add a run all bash script

---
+++ parse_all.sh
@@ -0,0 +1,17 @@
+tests=(
+    "element"
+    "empty"
+    "emptymemorydata"
+    "factorial"
+    "global"
+    "import"
+    "simple"
+    "start"
+    "stuff"
+)
+
+for test in "${tests[@]}"; do
+    echo "Running $test"
+    python3.6 main.py ./wasm_files/${test}/${test}.wasm
+    echo ""
+done
