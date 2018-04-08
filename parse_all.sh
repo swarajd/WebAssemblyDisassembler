da52dd9923d82524c143e404e50798ee53ba7c9d
Vincent Le <vinnyoodles@gmail.com>
2018-04-08 14:52:32 -0400

    Remove diffing from parse_all script

--- parse_all.sh
+++ parse_all.sh
@@ -13,8 +13,5 @@ tests=(
 
 for test in "${tests[@]}"; do
     echo "Running $test"
-    python3.6 main.py ./wasm_files/${test}/${test}.wasm > tmp_output && diff tmp_output wasm_files/${test}/${test}.wat
-    echo ""
+    python3.6 main.py ./wasm_files/${test}/${test}.wasm
 done
-
-rm tmp_output
