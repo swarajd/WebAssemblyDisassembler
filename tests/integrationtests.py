06f02c14269ae2f3b29d59fe46724698065fbdfa
Vincent Le <vinnyoodles@gmail.com>
2018-04-11 18:01:51 -0400

    Add WebAssembly specification test suite as wasm modules and refactor integration test #17

--- tests/integrationtests.py
+++ tests/integrationtests.py
@@ -7,47 +7,61 @@ import shlex, subprocess
 
 from main import disassemble
 
-files = [ 'element', 'empty', 'emptymemorydata', 'factorial', 'global', 'import', 'simple', 'start', 'stuff' ]
-
 class TestDissassembly(unittest.TestCase):
+    def setUp(self):
+        self.assembledFile    = open('assembled.wasm', 'w+')
+        self.assembledFile.close()
+        self.assembledFile    = open('assembled.wasm', 'rb')
+
+    def tearDown(self):
+        try:
+            self.assembledFile.close()
+            os.remove('assembled.wasm')
+            os.remove('disassembled.wat')
+        except FileNotFoundError:
+            pass
+
     def test_all(self):
-        assembledFile    = open('assembled.wasm', 'w+')
-        assembledFile.close()
-        assembledFile    = open('assembled.wasm', 'rb')
+        test_dir = './wasm_files'
+        for file in os.listdir(test_dir):
+            file_path = os.path.join(test_dir, file)
+            if not os.path.isdir(file_path):
+                continue
+            wasm_path = os.path.join(file_path, '{}.wasm'.format(file))
+            self.assert_disassemble(wasm_path)
 
-        for file in files: 
+    def test_spec(self):
+        test_dir = './spec/wasm'
+        for file in os.listdir(test_dir):
+            if not file.endswith('.wasm'):
+                continue
+            wasm_path = os.path.join(test_dir, file)
             print(file)
+            self.assert_disassemble(wasm_path)
 
-            wasm_path = os.path.join(dirname, 'wasm_files', file, '{}.wasm'.format(file))
+    def assert_disassemble(self, wasm_path):
+        wasm = open(wasm_path, 'rb')
+        expected_output_data  = wasm.read()
+        wasm.close()
 
-            wasm = open(wasm_path, 'rb')
+        disassembly = disassemble(wasm_path)
+        disassembledFile = open('disassembled.wat', 'w')
+        disassembledFile.write(disassembly)
+        disassembledFile.close()
 
-            expected_output_data  = wasm.read()
-            wasm.close()
+        args = shlex.split("wat2wasm -o assembled.wasm disassembled.wat ")
+        process = subprocess.Popen(args, stdout=subprocess.PIPE)
 
-            disassembly = disassemble(wasm_path)
-            disassembledFile = open('disassembled.wat', 'w')
-            disassembledFile.write(disassembly)
-            disassembledFile.close()
+        process_status = process.wait()
+        (output, err) = process.communicate()
 
-            args = shlex.split("wat2wasm -o assembled.wasm disassembled.wat ")
-            process = subprocess.Popen(args, stdout=subprocess.PIPE)
+        self.assembledFile.seek(0)
+        generated_output_data = self.assembledFile.read()
 
-            process_status = process.wait()
-            (output, err) = process.communicate()
+        exp_bytes = expected_output_data
+        gen_bytes = generated_output_data
 
-            assembledFile.seek(0)
-            generated_output_data = assembledFile.read()
-
-            exp_bytes = expected_output_data
-            gen_bytes = generated_output_data
-
-            self.assertEqual(gen_bytes, exp_bytes)
-
-
-        assembledFile.close()
-        os.remove('assembled.wasm')
-        os.remove('disassembled.wat')
+        self.assertEqual(gen_bytes, exp_bytes)
 
 if __name__ == '__main__':
     unittest.main()
